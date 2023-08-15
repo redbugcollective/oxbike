69763d549e5a659f671b5095c3f84b7b26bc5aaa
Craig Ambrose <craig@craigambrose.com>
2023-08-15 21:44:03 +1000

    added shirokuma

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more stuff
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { initWebAssembly, KeyPair } from 'shirokuma'
+
+await initWebAssembly()
+
+const keyPair = new KeyPair()
+console.log(keyPair.publicKey())
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

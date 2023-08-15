241ae637392db2602e300dad496691f96b07d46d
Craig Ambrose <craig@craigambrose.com>
2023-08-15 21:26:23 +1000

    changed to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more stuff
       </p>
     </>
   )

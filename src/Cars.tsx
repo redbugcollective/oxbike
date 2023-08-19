bd026bc0a4159f06b0a4f4bd91a2e810642e7342
Craig Ambrose <craig@craigambrose.com>
2023-08-19 11:23:28 +1000

    stopped loading local image asset

--- src/Cars.tsx
+++ src/Cars.tsx
@@ -1,26 +1,29 @@
 import Car from "./Car.tsx"
 import { Container, SimpleGrid } from "@chakra-ui/react"
-import carImage from "./assets/LightningMcQueen.webp"
 
 export default function Cars() {
-  const image = {
-    url: carImage,
-    alt: "A picture of Lightning McQueen used as a placeholder for the car",
-  }
-
   const cars = [
     {
-      image,
+      image: {
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Nissan_X-Trail_IV_IMG001.jpg/1920px-Nissan_X-Trail_IV_IMG001.jpg",
+        alt: "A grey nissan xtrail",
+      },
       make: "Nissan",
       model: "xTrail",
     },
     {
-      image,
+      image: {
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/2014_Honda_Jazz_VX_20221008.jpg/640px-2014_Honda_Jazz_VX_20221008.jpg",
+        alt: "A blue honda jazz",
+      },
       make: "Honda",
       model: "Jazz",
     },
     {
-      image,
+      image: {
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/2019_Toyota_Corolla_Altis_%28front%29.jpg/1920px-2019_Toyota_Corolla_Altis_%28front%29.jpg",
+        alt: "A white toyota corola",
+      },
       make: "Toyota",
       model: "Corola",
     },

ddb1b850d083516a281cff45a5825cc6dc43633e
Craig Ambrose <craig@craigambrose.com>
2023-09-25 13:58:06 +1000

    navbar stuff

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,14 +1,14 @@
-"use client";
+"use client"
 
-import { Box, Flex, useColorModeValue, Stack } from "@chakra-ui/react";
-import ColourModeSwitcher from "./ColorModeSwitcher.tsx";
+import { Box, Flex, useColorModeValue, Stack } from "@chakra-ui/react"
+import ColourModeSwitcher from "./ColorModeSwitcher.tsx"
 
 export default function Navbar() {
   return (
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <Box>OxBike</Box>
+          <Box>Panga Carsharing</Box>
 
           <Flex alignItems={"center"}>
             <Stack direction={"row"} spacing={7}>
@@ -18,5 +18,5 @@ export default function Navbar() {
         </Flex>
       </Box>
     </>
-  );
+  )
 }
